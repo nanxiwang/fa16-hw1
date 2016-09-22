71990826e7863974be1f62e08b4c20a69368b1f7
nanxiwang <nwang97@berkeley.edu>
2016-09-21 23:56:16 -0700

    completed hw

--- hw1.rb
+++ hw1.rb
@@ -1,17 +1,17 @@
 def squared_sum(a, b)
-  # Q1 CODE HERE
-
+  return (a+b)**2
 end
 
 def sort_array_plus_one(a)
-  # Q2 CODE HERE
-
-end
+  a = a.sort 
+  a.map do |element| 
+  	element + 1 
+  end 
+end 
 
 def combine_name(first_name, last_name)
-  # Q3 CODE HERE
-
-end
+	"#{first_name} #{last_name }"
+end 
 
 def blockin_time(a)
   # DO NOT EDIT THIS CODE BELOW
