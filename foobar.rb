71b05c3dba87aa755f7d8796413f60ae83abbcfb
nanxiwang <nwang97@berkeley.edu>
2016-09-21 23:56:49 -0700

    completed hw

--- foobar.rb
+++ foobar.rb
@@ -1,8 +1,17 @@
 class Foobar
 
   def self.baz(a)
-    # Class method
-    # Call with `Foobar.baz`
+	a.collect! do |el|
+		el.to_i
+	end 
+	
+	a.collect! do |i|
+		i = i + 2 
+	end 
+	a.delete_if {|x| x%2 == 0}
+	a.delete_if {|x| x> 10}
+	a = a.uniq
+	a.inject(0){|sum,x| sum + x }
   end
 end
 
